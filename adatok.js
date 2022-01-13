4bb35e2f3c2cff05653f07efce1c9d2daf1d2bc6
tatarmb4s <tatar.matyas.bence4s@gmail.com>
2022-01-13 10:53:28 +0100

    Coordinates values
    est. 1000 row of object code, just for the coordinates values.

--- adatok.js
+++ adatok.js
@@ -2,6 +2,430 @@
 
 var playersData = {
     player2 : {
+        coords : {
+            //a
+            a1: {
+                boat: "none",
+                fired: false,
+            },
+            a2: {
+                boat: "none",
+                fired: false,
+            },
+            a3: {
+                boat: "none",
+                fired: false,
+            },
+            a4: {
+                boat: "none",
+                fired: false,
+            },
+            a5: {
+                boat: "none",
+                fired: false,
+            },
+            a6: {
+                boat: "none",
+                fired: false,
+            },
+            a7: {
+                boat: "none",
+                fired: false,
+            },
+            a8: {
+                boat: "none",
+                fired: false,
+            },
+            a9: {
+                boat: "none",
+                fired: false,
+            },
+            a10: {
+                boat: "none",
+                fired: false,
+            },
+            //b
+            b1: {
+                boat: "none",
+                fired: false,
+            },
+            b2: {
+                boat: "none",
+                fired: false,
+            },
+            b3: {
+                boat: "none",
+                fired: false,
+            },
+            b4: {
+                boat: "none",
+                fired: false,
+            },
+            b5: {
+                boat: "none",
+                fired: false,
+            },
+            b6: {
+                boat: "none",
+                fired: false,
+            },
+            b7: {
+                boat: "none",
+                fired: false,
+            },
+            b8: {
+                boat: "none",
+                fired: false,
+            },
+            b9: {
+                boat: "none",
+                fired: false,
+            },
+            b10: {
+                boat: "none",
+                fired: false,
+            },
+        
+            //C
+            c1: {
+                boat: "none",
+                fired: false,
+            },
+            c2: {
+                boat: "none",
+                fired: false,
+            },
+            c3: {
+                boat: "none",
+                fired: false,
+            },
+            c4: {
+                boat: "none",
+                fired: false,
+            },
+            c5: {
+                boat: "none",
+                fired: false,
+            },
+            c6: {
+                boat: "none",
+                fired: false,
+            },
+            c7: {
+                boat: "none",
+                fired: false,
+            },
+            c8: {
+                boat: "none",
+                fired: false,
+            },
+            c9: {
+                boat: "none",
+                fired: false,
+            },
+            c10: {
+                boat: "none",
+                fired: false,
+            },
+        
+            //d
+            d1: {
+                boat: "none",
+                fired: false,
+            },
+            d2: {
+                boat: "none",
+                fired: false,
+            },
+            d3: {
+                boat: "none",
+                fired: false,
+            },
+            d4: {
+                boat: "none",
+                fired: false,
+            },
+            d5: {
+                boat: "none",
+                fired: false,
+            },
+            d6: {
+                boat: "none",
+                fired: false,
+            },
+            d7: {
+                boat: "none",
+                fired: false,
+            },
+            d8: {
+                boat: "none",
+                fired: false,
+            },
+            d9: {
+                boat: "none",
+                fired: false,
+            },
+            d10: {
+                boat: "none",
+                fired: false,
+            },
+            
+            //e
+            e1: {
+                boat: "none",
+                fired: false,
+            },
+            e2: {
+                boat: "none",
+                fired: false,
+            },
+            e3: {
+                boat: "none",
+                fired: false,
+            },
+            e4: {
+                boat: "none",
+                fired: false,
+            },
+            e5: {
+                boat: "none",
+                fired: false,
+            },
+            e6: {
+                boat: "none",
+                fired: false,
+            },
+            e7: {
+                boat: "none",
+                fired: false,
+            },
+            e8: {
+                boat: "none",
+                fired: false,
+            },
+            e9: {
+                boat: "none",
+                fired: false,
+            },
+            e10: {
+                boat: "none",
+                fired: false,
+            },
+            
+            //f
+            f1: {
+                boat: "none",
+                fired: false,
+            },
+            f2: {
+                boat: "none",
+                fired: false,
+            },
+            f3: {
+                boat: "none",
+                fired: false,
+            },
+            f4: {
+                boat: "none",
+                fired: false,
+            },
+            f5: {
+                boat: "none",
+                fired: false,
+            },
+            f6: {
+                boat: "none",
+                fired: false,
+            },
+            f7: {
+                boat: "none",
+                fired: false,
+            },
+            f8: {
+                boat: "none",
+                fired: false,
+            },
+            f9: {
+                boat: "none",
+                fired: false,
+            },
+            f10: {
+                boat: "none",
+                fired: false,
+            },
+        
+            
+            //g
+            g1: {
+                boat: "none",
+                fired: false,
+            },
+            g2: {
+                boat: "none",
+                fired: false,
+            },
+            g3: {
+                boat: "none",
+                fired: false,
+            },
+            g4: {
+                boat: "none",
+                fired: false,
+            },
+            g5: {
+                boat: "none",
+                fired: false,
+            },
+            g6: {
+                boat: "none",
+                fired: false,
+            },
+            g7: {
+                boat: "none",
+                fired: false,
+            },
+            g8: {
+                boat: "none",
+                fired: false,
+            },
+            g9: {
+                boat: "none",
+                fired: false,
+            },
+            g10: {
+                boat: "none",
+                fired: false,
+            },
+        
+            
+            //h
+            h1: {
+                boat: "none",
+                fired: false,
+            },
+            h2: {
+                boat: "none",
+                fired: false,
+            },
+            h3: {
+                boat: "none",
+                fired: false,
+            },
+            h4: {
+                boat: "none",
+                fired: false,
+            },
+            h5: {
+                boat: "none",
+                fired: false,
+            },
+            h6: {
+                boat: "none",
+                fired: false,
+            },
+            h7: {
+                boat: "none",
+                fired: false,
+            },
+            h8: {
+                boat: "none",
+                fired: false,
+            },
+            h9: {
+                boat: "none",
+                fired: false,
+            },
+            h10: {
+                boat: "none",
+                fired: false,
+            },
+        
+            
+            //i
+            i1: {
+                boat: "none",
+                fired: false,
+            },
+            i2: {
+                boat: "none",
+                fired: false,
+            },
+            i3: {
+                boat: "none",
+                fired: false,
+            },
+            i4: {
+                boat: "none",
+                fired: false,
+            },
+            i5: {
+                boat: "none",
+                fired: false,
+            },
+            i6: {
+                boat: "none",
+                fired: false,
+            },
+            i7: {
+                boat: "none",
+                fired: false,
+            },
+            i8: {
+                boat: "none",
+                fired: false,
+            },
+            i9: {
+                boat: "none",
+                fired: false,
+            },
+            i10: {
+                boat: "none",
+                fired: false,
+            },
+        
+            
+            //j
+            j1: {
+                boat: "none",
+                fired: false,
+            },
+            j2: {
+                boat: "none",
+                fired: false,
+            },
+            j3: {
+                boat: "none",
+                fired: false,
+            },
+            j4: {
+                boat: "none",
+                fired: false,
+            },
+            j5: {
+                boat: "none",
+                fired: false,
+            },
+            j6: {
+                boat: "none",
+                fired: false,
+            },
+            j7: {
+                boat: "none",
+                fired: false,
+            },
+            j8: {
+                boat: "none",
+                fired: false,
+            },
+            j9: {
+                boat: "none",
+                fired: false,
+            },
+            j10: {
+                boat: "none",
+                fired: false,
+            },  
+        },
         boats : {
             egyes:[
                 {
