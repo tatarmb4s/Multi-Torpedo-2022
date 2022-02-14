d6f67f4dd352e13fd544cde05f4f8e120d822c91
tatarmb4s <tatar.matyas.bence4s@gmail.com>
2022-02-14 09:06:39 +0100

    DragNDrop

---
+++ app/DragNDrop/main.js
@@ -0,0 +1,45 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+//Fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop trough empties and call drag events
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+// Darag funtcions
+function dragStart () {
+    console.log('start')
+    this.className += ' hold';
+    setTimeout(() => (this.className = ' invisible'), 0); 
+}
+
+function dragEnd() {
+    console.log('end')
+    this.className = "fill";
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    console.log('over');
+}
+function dragEnter(e) {
+    e.preventDefault();
+    console.log('enter');
+    this.className += ' hovered';
+}
+function dragLeave(e) {
+    console.log('leave');
+    this.className = 'empty';
+}
+function dragDrop(e) {
+    console.log('drop');
+    this.className = ' empty';
+    this.append(fill);
+}
